ead1a90b99fc9bc24a6bf55395a35a40b3df5b68
Lionel Rowe <lionel.rowe@gmail.com>
2020-08-06 23:00:52 +0100

    Updates

---
+++ precache-manifest.8ba0290ede812bc5819afb8af6f6609e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99f5012c6a3cdd20cef9e3ee8518347",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bffa21e3d8e2a8894318",
+    "url": "/static/css/main.07761089.chunk.css"
+  },
+  {
+    "revision": "0368eba7914f97e2e208",
+    "url": "/static/js/2.fc24ad78.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fc24ad78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffa21e3d8e2a8894318",
+    "url": "/static/js/main.5af92a6f.chunk.js"
+  },
+  {
+    "revision": "c57ead40ef1e7abaabde",
+    "url": "/static/js/runtime-main.b895bb24.js"
+  },
+  {
+    "revision": "bc70d0eef7b6d87d907c2baaa135ac6a",
+    "url": "/static/media/HKVariants.bc70d0ee.txt"
+  },
+  {
+    "revision": "f087cbfcc41bcbf46a577c3256c8699a",
+    "url": "/static/media/HKVariantsRev.f087cbfc.txt"
+  },
+  {
+    "revision": "77cec53fec60331ee98f988b99c6e4f6",
+    "url": "/static/media/HKVariantsRevPhrases.77cec53f.txt"
+  },
+  {
+    "revision": "b0943f47b6c183f76e4cd5fe0fc1039c",
+    "url": "/static/media/JPShinjitaiCharacters.b0943f47.txt"
+  },
+  {
+    "revision": "39dc8ef0bdf82768ab297a5bcf8301e2",
+    "url": "/static/media/JPShinjitaiPhrases.39dc8ef0.txt"
+  },
+  {
+    "revision": "dea5d018c5f1051dfa3ca9b92b7b5860",
+    "url": "/static/media/JPVariants.dea5d018.txt"
+  },
+  {
+    "revision": "fb34b4610c27b043086f846ec40ab913",
+    "url": "/static/media/JPVariantsRev.fb34b461.txt"
+  },
+  {
+    "revision": "d347b99bd61b7db11407a8c590f44539",
+    "url": "/static/media/STCharacters.d347b99b.txt"
+  },
+  {
+    "revision": "43cb99739eb0033def5f4b23841822eb",
+    "url": "/static/media/STPhrases.43cb9973.txt"
+  },
+  {
+    "revision": "dfe1cbf788d8138de368812137eb4e6d",
+    "url": "/static/media/TSCharacters.dfe1cbf7.txt"
+  },
+  {
+    "revision": "abb6ae03e4be4c05e0115c9c05d9cf4c",
+    "url": "/static/media/TSPhrases.abb6ae03.txt"
+  },
+  {
+    "revision": "38ad6e291b15a2276fb4055e02e2108d",
+    "url": "/static/media/TWPhrasesIT.38ad6e29.txt"
+  },
+  {
+    "revision": "fd198a5445ad2b1689786c101a82ea19",
+    "url": "/static/media/TWPhrasesName.fd198a54.txt"
+  },
+  {
+    "revision": "6a29ba7cbb3eb535e13994e751e80602",
+    "url": "/static/media/TWPhrasesOther.6a29ba7c.txt"
+  },
+  {
+    "revision": "68fdb5ae79323204d00b74ba2cde28f8",
+    "url": "/static/media/TWPhrasesRev.68fdb5ae.txt"
+  },
+  {
+    "revision": "86fbbc84f91254629c3cf7e73efd79f7",
+    "url": "/static/media/TWVariants.86fbbc84.txt"
+  },
+  {
+    "revision": "a266f0ed48b956063e521f97cad142e5",
+    "url": "/static/media/TWVariantsRev.a266f0ed.txt"
+  },
+  {
+    "revision": "6c5e1788aa9e5adeda0ba36a28ae4871",
+    "url": "/static/media/TWVariantsRevPhrases.6c5e1788.txt"
+  }
+]);
